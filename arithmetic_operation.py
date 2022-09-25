60b42a03558dc92dc91c47d2a4835340548b2f38
SangJun <73101224+goodpinokio@users.noreply.github.com>
2022-09-25 21:45:02 +0900

    arithmetic operation(python ex)

---
+++ arithmetic_operation.py
@@ -0,0 +1,40 @@
+# 정수
+a= 777
+print(a)
+
+a = a+ 5
+print(a)
+
+# 실수
+a =5.
+print(a)
+
+a=-.7
+print(a)
+
+#지수 
+#1,000,000,000의 지수 표현 방식
+a=1e9
+print(a)
+
+a=75.25e1
+print(a)
+
+a=3954e-3
+print(a)
+
+#실수 round()
+#반올림
+b=round(123.456,2)
+print(b)
+
+a=0.3+0.6
+if round(a,4) == 0.9:
+    print(True)
+else:
+    print(False)
+
+# 나누기 연산자(/)
+# 나머지 연산자(%)
+# 몫 연산자(//)
+# 거듭 제곱 연산자(**)
