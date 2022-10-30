2374197432d4048b663306ff922d9fba489c6657
SangJun <73101224+goodpinokio@users.noreply.github.com>
2022-10-30 09:51:14 +0900

    선택정렬 삽입정렬

--- sorting_ex.py
+++ sorting_ex.py
@@ -12,3 +12,12 @@ for i in range(len(array)):
 
 print(array)
 
+#삽입정렬
+for i in range(1,len(array)):
+    for j in range(i,0,-1):
+        if array[j] < array[j-1]:
+            array[j],array[j-1] = array[j-1], array[j]
+        else:
+            break
+
+print(array)
