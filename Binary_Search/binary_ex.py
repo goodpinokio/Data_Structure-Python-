fdbac3e2266bfa256f15bb5d1fbe572a441e3198
SangJun <73101224+goodpinokio@users.noreply.github.com>
2022-11-14 09:23:43 +0900

    이진탐색 알고리즘 예시코드

--- Binary_Search/binary_ex.py
+++ Binary_Search/binary_ex.py
@@ -18,6 +18,6 @@ array = list(map(int, input().split()))
 result = binary_search(array,target,0,n-1)
 
 if result == None:
-    print("원솨 존재하지 않습니다.")
+    print("원소가 존재하지 않습니다.")
 else:
     print(result + 1) 
